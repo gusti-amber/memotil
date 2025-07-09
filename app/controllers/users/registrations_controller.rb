4365a9b872cafa292506bdccf01be1578d65dccf
Amber <amberseia1212@gmail.com>
2025-07-09 09:05:34 +0900

    feat: ユーザー登録用とログイン/ログアウト用のコントローラーのcreateアクションをオーバーライドし、current_or_guest_userを実行する処理を追加 ✨

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    current_or_guest_user
+  end
 
   # GET /resource/edit
   # def edit
