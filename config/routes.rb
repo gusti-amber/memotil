ee99482af5d3f16a9af114737d75d6205c49b453
Amber <amberseia1212@gmail.com>
2025-07-10 10:38:50 +0900

    feat: ゲストユーザー作成用のルーティングを追加 ✨

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
+  
+  # ゲストユーザー作成用ルート
+  devise_scope :user do
+    post '/users/guest', to: 'users/sessions#create_guest', as: :guest_session
+  end
+
   root "static_pages#top"
 end
