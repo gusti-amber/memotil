60eb3a121ae45ee15bc92d39e3fde81eb50a23c1
Amber <amberseia1212@gmail.com>
2025-07-11 16:46:24 +0900

    feat: Userモデルに`has_many :tasks`を追加 ✨

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
+  has_many :tasks, dependent: :destroy
   # カスタムバリデーション
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
 end
