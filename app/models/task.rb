2d2880095e61f6ea37e6b0d1ae40e0cb938b8d22
Amber <amberseia1212@gmail.com>
2025-07-11 16:57:41 +0900

    feat: Taskモデルにバリデーションを追加 ✨

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ApplicationRecord
   belongs_to :user
 
   enum status: { todo: 0, doing: 1, done: 2 }
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :status, presence: true
 end
