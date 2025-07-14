f2db090f12fb4674323fd89870566583245588be
Amber <amberseia1212@gmail.com>
2025-07-14 10:42:04 +0900

    fix: `rails db:migrate`実行時にスキーマを変更 🔧

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_11_072230) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_14_013608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,6 +27,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_11_072230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "tasktags", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_tasktags_on_tag_id"
+    t.index ["task_id"], name: "index_tasktags_on_task_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +52,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_11_072230) do
   end
 
   add_foreign_key "tasks", "users"
+  add_foreign_key "tasktags", "tags"
+  add_foreign_key "tasktags", "tasks"
 end
