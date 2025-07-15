cb91fb427189b44a6b15c7ceeacd7ddac4e0eb00
Amber <amberseia1212@gmail.com>
2025-07-15 15:11:43 +0900

    fix: Taskデーブルのstatusカラムのデフォルト値が0に変更 🔧

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_14_013608) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_15_060742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_14_013608) do
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
-    t.integer "status", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
