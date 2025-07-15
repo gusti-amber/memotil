7a6cddc181f7dd7a6cea6b5ac2adf3aab0f0e7f7
Amber <amberseia1212@gmail.com>
2025-07-15 16:17:43 +0900

    refactor: 各ファイルのlintエラーをまとめて修正 ♻️

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,9 @@ class TasksController < ApplicationController
 
   def create
     @task = current_or_guest_user.tasks.build(task_params)
-    
+
     if @task.save
-      redirect_to @task, notice: 'タスクが正常に作成されました。'
+      redirect_to @task, notice: "タスクが正常に作成されました。"
     else
       render :new, status: :unprocessable_entity
     end
