c26f59239a41f0c9e49dc763c2b2243c7db8771b
Amber <amberseia1212@gmail.com>
2025-07-17 17:17:33 +0900

    fix: todoのmigrationファイルを変更(bodyにnull: false, doneにdefault: false, null: false) 🔧

--- db/migrate/20250717081334_create_todos.rb
+++ db/migrate/20250717081334_create_todos.rb
@@ -2,8 +2,8 @@ class CreateTodos < ActiveRecord::Migration[7.2]
   def change
     create_table :todos do |t|
       t.references :task, null: false, foreign_key: true
-      t.string :body
-      t.boolean :done
+      t.string :body, null: false
+      t.boolean :done, default: false, null: false
 
       t.timestamps
     end
