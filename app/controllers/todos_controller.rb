d3f6ccebbe65b7d0ee1217fa7cb25fc90524cea4
Amber <amberseia1212@gmail.com>
2025-07-23 09:30:16 +0900

    feat: todoコントローラーを作成し、updateアクションを記述 ✨

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,14 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:update]
+
+  def update
+    @todo.update(done: !@todo.done?) # 現在はtodoで変更できることはdoneカラムのトグルのみだが、今後bodyカラムも更新をする場合はここを変更する
+    render partial: "todos/todo", locals: { todo: @todo }
+  end
+
+  private
+
+  def set_todo
+    @todo = current_or_guest_user.tasks.find(params[:task_id]).todos.find(params[:id])
+  end
+end 
