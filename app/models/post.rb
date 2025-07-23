ec1fcbb27b27cad87df460016e92e1c6da09119f
Amber <amberseia1212@gmail.com>
2025-07-23 15:27:51 +0900

    refactor: delegated_typeのタイプミスを訂正 ♻️

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :task
   belongs_to :user
-  delegate_type :postable, types: %w[Memo], dependent: :destroy
+  delegated_type :postable, types: %w[Memo], dependent: :destroy
 end
