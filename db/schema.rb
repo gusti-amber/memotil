505238a21867cadbd3118d30a7f28f215fcd8900
Amber <amberseia1212@gmail.com>
2025-07-23 15:37:48 +0900

    fix: db:migrate実行時にスキーマを変更 🔧

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_23_061729) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_23_063543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,8 +23,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_23_061729) do
   create_table "posts", force: :cascade do |t|
     t.bigint "task_id", null: false
     t.bigint "user_id", null: false
-    t.string "postable_type"
-    t.integer "postable_id"
+    t.string "postable_type", null: false
+    t.integer "postable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_posts_on_task_id"
