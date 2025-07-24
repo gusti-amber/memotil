342770ac4cded12110452e33576510179c995061
Amber <amberseia1212@gmail.com>
2025-07-24 18:05:13 +0900

    fix: delegated_typeのタイプをMemoからTextPostに変更 👍

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :task
   belongs_to :user
-  delegated_type :postable, types: %w[Memo], dependent: :destroy
+  delegated_type :postable, types: %w[TextPost], dependent: :destroy
 end
