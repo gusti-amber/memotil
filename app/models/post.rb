cb1ddd53c0b58bc80d26744ae1a4fd9a594b47d5
Amber <amberseia1212@gmail.com>
2025-07-26 20:01:31 +0900

    feat: Postモデルにpostable属性のネストされた属性を受け入れる機能を追加 ✨

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :task
   belongs_to :user
   delegated_type :postable, types: %w[TextPost], dependent: :destroy
+  accepts_nested_attributes_for :postable
 end
