19e9a1e03f675285a4d3ffb3b4609965dabf822d
Amber <amberseia1212@gmail.com>
2025-08-01 14:45:33 +0900

    feat: tailwind設定ファイルに配色を追加 ✨

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,25 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        custom: {
+          cream: "#FFF8E8",
+          beige: "#F7EED3",
+          sage: "#AAB396",
+          "sage-dark": "#8A9A7A",
+          brown: "#674636",
+        },
       },
     },
   },
@@ -18,5 +27,5 @@ module.exports = {
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
-  ]
-}
+  ],
+};
