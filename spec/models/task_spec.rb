e482f924ebf99d06ff04060df5e97b6e8269ef5c
Amber <amberseia1212@gmail.com>
2025-08-05 13:36:54 +0900

    test: タスクモデルのタグ数バリデーションに関するRSpecテストを追加 💚

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -32,4 +32,41 @@ RSpec.describe Task, type: :model do
       expect(task_with_long_title.errors[:title]).to include('is too long (maximum is 255 characters)')
     end
   end
+
+  describe 'タグの個数制限バリデーション' do
+    let(:user) { create(:user) }
+    let(:tags) { create_list(:tag, 6) }
+
+    it 'タグが5個以下の場合は有効' do
+      task = build(:task, user: user, tag_ids: tags.first(5).map(&:id))
+      expect(task).to be_valid
+      expect(task.errors[:tag_ids]).to be_empty
+    end
+
+    it 'タグが6個の場合は無効' do
+      task = build(:task, user: user, tag_ids: tags.map(&:id))
+      expect(task).to be_invalid
+      expect(task.errors[:tag_ids]).to include('は最大5個まで選択できます')
+    end
+
+    it 'タグが選択されていない場合は有効' do
+      task = build(:task, user: user, tag_ids: [])
+      expect(task).to be_valid
+      expect(task.errors[:tag_ids]).to be_empty
+    end
+
+    it 'タグが選択されていない場合（nil）は有効' do
+      task = build(:task, user: user, tag_ids: nil)
+      expect(task).to be_valid
+      expect(task.errors[:tag_ids]).to be_empty
+    end
+
+    it 'タグが7個の場合は無効' do
+      extra_tags = create_list(:tag, 1)
+      all_tags = tags + extra_tags
+      task = build(:task, user: user, tag_ids: all_tags.map(&:id))
+      expect(task).to be_invalid
+      expect(task.errors[:tag_ids]).to include('は最大5個まで選択できます')
+    end
+  end
 end
