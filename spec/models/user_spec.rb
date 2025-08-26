38e13bde0c57101e905518196d34aaaa438a4ec7
Amber <amberseia1212@gmail.com>
2025-08-26 18:00:33 +0900

    test: Userモデルスペックのuser.nameの値の統一 💚

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
     describe 'name' do
       context '存在性' do
         it 'nameが存在する場合は有効' do
-          user.name = 'userテスト'
+          user.name = 'user_name'
           expect(user).to be_valid, 'nameが存在する場合は有効である必要があります'
         end
 
@@ -26,18 +26,18 @@ RSpec.describe User, type: :model do
 
       context '文字数制限' do
         it '2文字以上20文字以内の場合は有効' do
-          user.name = 'テスト'
+          user.name = 'user_name'
           expect(user).to be_valid, 'nameが2文字以上20文字以内の場合は有効である必要があります'
         end
 
         it '1文字の場合は無効' do
-          user.name = 'A'
+          user.name = 'a'
           expect(user).not_to be_valid, 'nameが1文字の場合は無効である必要があります'
           expect(user.errors[:name]).to include('は2文字以上で入力してください')
         end
 
         it '21文字以上の場合は無効' do
-          user.name = 'A' * 21
+          user.name = 'a' * 21
           expect(user).not_to be_valid, 'nameが21文字以上の場合は無効である必要があります'
           expect(user.errors[:name]).to include('は20文字以下で入力してください')
         end
@@ -47,14 +47,14 @@ RSpec.describe User, type: :model do
     describe 'password' do
       context '文字数制限' do
         it '8文字以上128文字以内の場合は有効' do
-          user.password = '12345678'
-          user.password_confirmation = '12345678'
+          user.password = 'a' * 8
+          user.password_confirmation = 'a' * 8
           expect(user).to be_valid, 'passwordが8文字以上128文字以内の場合は有効である必要があります'
         end
 
         it '7文字以下の場合は無効' do
-          user.password = '1234567'
-          user.password_confirmation = '1234567'
+          user.password = 'a' * 7
+          user.password_confirmation = 'a' * 7
           expect(user).not_to be_valid, 'passwordが7文字以下の場合は無効である必要があります'
           expect(user.errors[:password]).to include('は8文字以上で入力してください')
         end
