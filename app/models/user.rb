e2afc828dd9bf06506a1fc59a1e059e572cb7dbe
Amber <amberseia1212@gmail.com>
2025-08-26 14:05:54 +0900

    fix: Userモデルのnameバリデーションの最大文字数を20に変更 👍

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :posts, dependent: :destroy
   # カスタムバリデーション
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :password, presence: true, length: { minimum: 6 }
 end
