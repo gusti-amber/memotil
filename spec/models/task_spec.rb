5c4816fcd4c2074e500981acc1fdb69368e78c96
Amber <amberseia1212@gmail.com>
2025-08-26 11:32:07 +0900

    test: Taskモデルのtitleおよびstatusバリデーションに関するテストを追加・整理 💚

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,33 +2,59 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'バリデーション' do
-    it '設定したすべてのバリデーションが機能しているか' do
-      task = build(:task)
-      expect(task).to be_valid
-      expect(task.errors).to be_empty
+    let(:user) { create(:user) }
+    let(:task) { build(:task, user: user) }
+
+    describe 'title' do
+      context '存在性' do
+        it 'titleが存在する場合は有効' do
+          task.title = 'taskテスト'
+          expect(task).to be_valid
+        end
+
+        it 'titleが空の場合は無効' do
+          task.title = ''
+          expect(task).not_to be_valid
+          expect(task.errors[:title]).to include('を入力してください')
+        end
+
+        it 'titleがnilの場合は無効' do
+          task.title = nil
+          expect(task).not_to be_valid
+          expect(task.errors[:title]).to include('を入力してください')
+        end
+      end
+
+      context '文字数制限' do
+        it '2文字以上255文字以内の場合は有効' do
+          task.title = 'テスト'
+          expect(task).to be_valid
+        end
+
+        it '1文字の場合は無効' do
+          task.title = 'A'
+          expect(task).not_to be_valid
+          expect(task.errors[:title]).to include('は2文字以上で入力してください')
+        end
+
+        it '256文字以上の場合は無効' do
+          task.title = 'A' * 256
+          expect(task).not_to be_valid
+          expect(task.errors[:title]).to include('は255文字以下で入力してください')
+        end
+      end
     end
 
-    it 'titleがない場合、バリデーションが機能し、invalidになる' do
-      task_without_title = build(:task, title: nil)
-      expect(task_without_title).to be_invalid
-      expect(task_without_title.errors[:title]).to include("を入力してください")
-    end
+    describe 'status' do
+      it 'statusが存在する場合は有効' do
+        task.status = 'todo'
+        expect(task).to be_valid
+      end
 
-    it 'statusがない場合、バリデーションが機能し、invalidになる' do
-      task_without_status = build(:task, status: nil)
-      expect(task_without_status).to be_invalid
-    end
-
-    it 'titleが1文字の場合、バリデーションが機能し、invalidになる' do
-      task_with_short_title = build(:task, title: 'A')
-      expect(task_with_short_title).to be_invalid
-      expect(task_with_short_title.errors[:title]).to include('は2文字以上で入力してください')
-    end
-
-    it 'titleが256文字の場合、バリデーションが機能し、invalidになる' do
-      task_with_long_title = build(:task, title: 'A' * 256)
-      expect(task_with_long_title).to be_invalid
-      expect(task_with_long_title.errors[:title]).to include('は255文字以下で入力してください')
+      it 'statusがnilの場合は無効' do
+        task.status = nil
+        expect(task).not_to be_valid
+      end
     end
   end
 
