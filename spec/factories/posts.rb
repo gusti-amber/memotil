146f5a75a7118c94f1f1371e29a6e991b01f2ae7
Amber <amberseia1212@gmail.com>
2025-08-27 19:52:25 +0900

    fix: Postファクトリでのpostableアソシエーションを修正 💚

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :post do
     association :user
     association :task
-    postable_type { "TextPost" }
-    postable_id { create(:text_post).id }
+    association :postable
   end
 end
