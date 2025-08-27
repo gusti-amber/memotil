944e0f07e529f1c867605c9684de09b41d6fe93b
Amber <amberseia1212@gmail.com>
2025-08-27 14:37:19 +0900

    test: Todoモデルスペックのバリデーションテストにエラーメッセージを追加 💚

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe Todo, type: :model do
     describe 'body' do
       context '存在性' do
         it 'bodyが存在する場合は有効' do
-          todo.body = 'todoテスト'
-          expect(todo).to be_valid
+          todo.body = 'todo_body'
+          expect(todo).to be_valid, 'bodyが存在する場合は有効である必要があります'
         end
 
         it 'bodyが空の場合は無効' do
           todo.body = ''
-          expect(todo).not_to be_valid
+          expect(todo).not_to be_valid, 'bodyが空の場合は無効である必要があります'
           expect(todo.errors[:body]).to include('を入力してください')
         end
 
         it 'bodyがnilの場合は無効' do
           todo.body = nil
-          expect(todo).not_to be_valid
+          expect(todo).not_to be_valid, 'bodyがnilの場合は無効である必要があります'
           expect(todo.errors[:body]).to include('を入力してください')
         end
       end
@@ -28,12 +28,12 @@ RSpec.describe Todo, type: :model do
       context '文字数制限' do
         it '255文字以内の場合は有効' do
           todo.body = 'a' * 255
-          expect(todo).to be_valid
+          expect(todo).to be_valid, 'bodyが255文字以内の場合は有効である必要があります'
         end
 
         it '256文字以上の場合は無効' do
           todo.body = 'a' * 256
-          expect(todo).not_to be_valid
+          expect(todo).not_to be_valid, 'bodyが256文字以上の場合は無効である必要があります'
           expect(todo.errors[:body]).to include('は255文字以下で入力してください')
         end
       end
