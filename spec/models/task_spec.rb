afbf081bee966e32daee9fa727ee70f03bd68357
Amber <amberseia1212@gmail.com>
2025-08-27 15:53:08 +0900

    test: Taskモデルスペックのバリデーションテストにエラーメッセージを追加 💚

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,38 +8,38 @@ RSpec.describe Task, type: :model do
     describe 'title' do
       context '存在性' do
         it 'titleが存在する場合は有効' do
-          task.title = 'taskテスト'
-          expect(task).to be_valid
+          task.title = 'task_title'
+          expect(task).to be_valid, 'titleが存在する場合は有効である必要があります'
         end
 
         it 'titleが空の場合は無効' do
           task.title = ''
-          expect(task).not_to be_valid
+          expect(task).not_to be_valid, 'titleが空の場合は無効である必要があります'
           expect(task.errors[:title]).to include('を入力してください')
         end
 
         it 'titleがnilの場合は無効' do
           task.title = nil
-          expect(task).not_to be_valid
+          expect(task).not_to be_valid, 'titleがnilの場合は無効である必要があります'
           expect(task.errors[:title]).to include('を入力してください')
         end
       end
 
       context '文字数制限' do
         it '2文字以上255文字以内の場合は有効' do
-          task.title = 'テスト'
-          expect(task).to be_valid
+          task.title = 'task_title'
+          expect(task).to be_valid, 'titleが2文字以上255文字以内の場合は有効である必要があります'
         end
 
         it '1文字の場合は無効' do
-          task.title = 'A'
-          expect(task).not_to be_valid
+          task.title = 'a'
+          expect(task).not_to be_valid, 'titleが1文字の場合は無効である必要があります'
           expect(task.errors[:title]).to include('は2文字以上で入力してください')
         end
 
         it '256文字以上の場合は無効' do
-          task.title = 'A' * 256
-          expect(task).not_to be_valid
+          task.title = 'a' * 256
+          expect(task).not_to be_valid, 'titleが256文字以上の場合は無効である必要があります'
           expect(task.errors[:title]).to include('は255文字以下で入力してください')
         end
       end
@@ -48,12 +48,12 @@ RSpec.describe Task, type: :model do
     describe 'status' do
       it 'statusが存在する場合は有効' do
         task.status = 'todo'
-        expect(task).to be_valid
+        expect(task).to be_valid, 'statusが存在する場合は有効である必要があります'
       end
 
       it 'statusがnilの場合は無効' do
         task.status = nil
-        expect(task).not_to be_valid
+        expect(task).not_to be_valid, 'statusがnilの場合は無効である必要があります'
       end
     end
   end
@@ -65,19 +65,19 @@ RSpec.describe Task, type: :model do
 
     it 'Todoが存在しない場合は有効' do
       task = build(:task, user: user)
-      expect(task).to be_valid
+      expect(task).to be_valid, 'Todoが存在しない場合は有効である必要があります'
     end
 
     it 'Todoが最大数以下の場合は有効' do
       task = build(:task, user: user)
       create_list(:todo, max_todos, task: task)
-      expect(task).to be_valid
+      expect(task).to be_valid, 'Todoが最大数以下の場合は有効である必要があります'
     end
 
     it 'Todoが最大数を超える場合は無効' do
       task = build(:task, user: user)
       create_list(:todo, max_todos + 1, task: task)
-      expect(task).not_to be_valid
+      expect(task).not_to be_valid, 'Todoが最大数を超える場合は無効である必要があります'
       expect(task.errors[:todos]).to include(error_message)
     end
   end
@@ -89,24 +89,24 @@ RSpec.describe Task, type: :model do
 
     it 'タグが選択されていない場合（空配列）は有効' do
       task = build(:task, user: user, tag_ids: [])
-      expect(task).to be_valid
+      expect(task).to be_valid, 'タグが選択されていない場合（空配列）は有効である必要があります'
     end
 
     it 'タグが選択されていない場合（nil）は有効' do
       task = build(:task, user: user, tag_ids: nil)
-      expect(task).to be_valid
+      expect(task).to be_valid, 'タグが選択されていない場合（nil）は有効である必要があります'
     end
 
     it 'タグが最大数以下の場合は有効' do
       tags = create_list(:tag, max_tags)
       task = build(:task, user: user, tag_ids: tags.map(&:id))
-      expect(task).to be_valid
+      expect(task).to be_valid, 'タグが最大数以下の場合は有効である必要があります'
     end
 
     it 'タグが最大数を超える場合は無効' do
       tags = create_list(:tag, max_tags + 1)
       task = build(:task, user: user, tag_ids: tags.map(&:id))
-      expect(task).to be_invalid
+      expect(task).to be_invalid, 'タグが最大数を超える場合は無効である必要があります'
       expect(task.errors[:tag_ids]).to include(error_message)
     end
   end
