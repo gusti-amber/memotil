e88c991d9d35110db0c458ae0e7ed38e381b30aa
Amber <amberseia1212@gmail.com>
2025-09-01 10:23:07 +0900

    refactor: 各ファイルのlintエラーをまとめて修正 ♻️

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -16,26 +16,26 @@ RSpec.describe 'Posts', type: :system do
         it 'TextPostが正常に作成される' do
           fill_in 'post[postable_attributes][body]', with: 'test_text_post'
           click_button '投稿'
-  
+
           expect(page).to have_content('test_text_post')
           # expect(page).to have_content('コメントが投稿されました。')
           expect(current_path).to eq task_path(task)
         end
       end
-  
+
       context 'バリデーションエラーが発生する場合' do
         it 'bodyが空の場合、エラーメッセージが表示される' do
           fill_in 'post[postable_attributes][body]', with: ''
           click_button '投稿'
-  
+
           # expect(page).to have_content('コメントの内容が無効です。')
           expect(current_path).to eq task_path(task)
         end
-  
+
         it 'bodyが501文字以上の場合、エラーメッセージが表示される' do
           fill_in 'post[postable_attributes][body]', with: 'a' * 501
           click_button '投稿'
-  
+
           # expect(page).to have_content('コメントの内容が無効です。')
           expect(current_path).to eq task_path(task)
         end
@@ -52,7 +52,7 @@ RSpec.describe 'Posts', type: :system do
         sign_in user
         visit task_path(task)
       end
-      
+
       context 'TextPostが存在する場合' do
         it 'TextPost一覧が正しく表示される' do
           expect(page).to have_content('test_text_post')
