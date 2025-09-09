52b41dbf35518fdc278d7a050807f3b775cab7e6
Amber <amberseia1212@gmail.com>
2025-09-09 20:20:32 +0900

    test: 投稿削除機能のシステムテストを追加 💚

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -62,4 +62,50 @@ RSpec.describe 'Posts', type: :system do
       end
     end
   end
+
+  describe '投稿削除' do
+    let(:task) { create(:task, user: user) }
+    let!(:post) { create(:post, user: user, task: task, postable: create(:text_post, body: 'test_text_post')) }
+
+    before do
+      sign_in user
+      visit task_path(task)
+    end
+
+    context '正常な削除の場合' do
+      it '右クリックでコンテキストメニューが表示される' do
+        # 投稿のchat-bubble要素を右クリック
+        chat_bubble = find('.chat-bubble')
+        chat_bubble.right_click
+
+        # 削除ボタンをクリック
+        accept_confirm do
+          within('[data-post-context-menu-target="menu"]') do
+            click_link '削除'
+          end
+        end
+
+        # 投稿が削除されることを確認
+        expect(page).not_to have_content('test_text_post')
+        expect(current_path).to eq task_path(task)
+      end
+
+      it '削除確認ダイアログでキャンセルした場合、投稿が削除されない' do
+        # 投稿のchat-bubble要素を右クリック
+        chat_bubble = find('.chat-bubble')
+        chat_bubble.right_click
+
+        # 削除ボタンをクリック
+        dismiss_confirm do
+          within('[data-post-context-menu-target="menu"]') do
+            click_link '削除'
+          end
+        end
+
+        # 投稿が残っていることを確認
+        expect(page).to have_content('test_text_post')
+        expect(current_path).to eq task_path(task)
+      end
+    end
+  end
 end
