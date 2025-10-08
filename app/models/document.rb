90afbe21c0e9feec938a4335cdf9cc286eb7d5d5
Amber <amberseia1212@gmail.com>
2025-10-08 09:53:36 +0900

    feat: documentモデルにURL形式のバリデーションを追加 ✨

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,20 @@ class Document < ApplicationRecord
   has_many :document_posts, dependent: :destroy
 
   validates :url, presence: true, uniqueness: true
+  validate :url_format_validation
+
+  private
+
+  def url_format_validation
+    return if url.blank?
+
+    begin
+      uri = URI.parse(url)
+      unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+        errors.add(:url, :invalid_protocol)
+      end
+    rescue URI::InvalidURIError
+      errors.add(:url, :invalid_url_format)
+    end
+  end
 end
