dfcb21591238210ec796de48779b11bc54dfeeab
Amber <amberseia1212@gmail.com>
2025-11-03 14:52:28 +0900

    feat: Githubリポジトリから情報を取得するメソッドを実装 ✨
    - Githubトークンからユーザー認証するメソッド
    - 認証ユーザーのGithubからリポジトリ一覧を取得するメソッド
    - 指定したリポジトリからREADME.mdの情報を取得するメソッド

---
+++ app/services/github_service.rb
@@ -0,0 +1,22 @@
+class GithubService
+  def initialize(access_token)
+    @client = Octokit::Client.new(access_token: access_token)
+  end
+
+  def list_repositories
+    # 🎓 GitHub API のリポジトリ一覧取得方法: https://octokit.github.io/octokit.rb/Octokit/Client/Repositories.html#repositories-instance_method
+    @client.repos(nil, per_page: 100)
+  end
+
+  def fetch_readme(repo)
+    content = @client.contents(repo, path: "README.md")
+
+    # 取得したREADME.mdをBase64でデコードし、UTF-8でエンコード
+    body = Base64.decode64(content[:content]).force_encoding('UTF-8')
+    { sha: content[:sha], body: body }
+  rescue Octokit::NotFound
+    { sha: nil, body: "" }
+  end
+end
+
+
