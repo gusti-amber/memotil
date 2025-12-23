1fa93926fb72c8b3b03ac121e4e80134ead8c813
Amber <amberseia1212@gmail.com>
2025-12-23 11:17:19 +0900

    test: ログイン状態でログイン画面へ遷移する場合のテストに、サクセスメッセージの表示検証を追加 💚

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -215,8 +215,12 @@ RSpec.describe 'Users', type: :system do
       it 'ルートページにリダイレクトされる' do
         visit new_user_session_path
 
-        # 既にログインしている場合はルートページにリダイレクト
+        # 既にログインしている場合はタスク一覧画面にリダイレクト
         expect(page).to have_current_path(tasks_path)
+
+        # サクセスメッセージの表示
+        expect(page).to have_css('.alert.alert-error')
+        expect(page).to have_content('すでにログインしています')
       end
     end
   end
