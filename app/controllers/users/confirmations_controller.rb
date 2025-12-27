bc43ffd7b35432975576dbdf3e9a225bb0c8ea29
Amber <amberseia1212@gmail.com>
2025-12-27 15:38:17 +0900

    feat: サインアップ後の確認メール内のリンクをクリックした際にはログイン画面へリダイレクトするように設定 ✨
    - メールアドレスの確認処理の前に、確認メールがメールアドレス変更用のものか確認する処理を追加
    
    - 確認メール内のリンクをクリックした時の、リダイレクト先をメールの種類により条件分岐

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,9 +2,15 @@
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def show
+    # 🎓 Devise デフォルトの記述: https://github.com/heartcombo/devise/blob/main/app/controllers/devise/confirmations_controller.rb
+    # 確認処理の前に、メールアドレス変更用の確認メールかどうかを判定(確認処理が完了すると、unconfirmed_emailがnilになるため)
+    # unconfirmed_emailが存在する場合はメールアドレス変更用のメールと判定
+    self.resource = resource_class.find_by_confirmation_token(params[:confirmation_token])
+    @is_email_change = resource&.unconfirmed_email.present?
+
+    super
+  end
 
   # POST /resource/confirmation
   # def create
@@ -14,8 +20,15 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   protected
 
   # 確認後のリダイレクト先を指定するメソッド
-  # 元いた画面（stored_location_for）があればそこへ、なければタスク一覧画面へリダイレクト
+  # - メールアドレス変更用の確認メール → 元いた画面があればそこへ、なければタスク一覧画面へ
+  # - メールアドレス登録用の確認メール（サインアップ時）→ ログイン画面へ
   def after_confirmation_path_for(resource_name, resource)
-    stored_location_for(resource) || tasks_path
+    if @is_email_change
+      # メールアドレス変更用の確認メールの場合
+      stored_location_for(resource) || tasks_path
+    else
+      # メールアドレス登録用の確認メール（サインアップ時）の場合
+      new_user_session_path
+    end
   end
 end
