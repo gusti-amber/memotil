a4577338129d930adf4d593c933aff39d8076763
Amber <amberseia1212@gmail.com>
2026-01-10 09:54:58 +0900

    refactor: 既存のtils#new, createアクションをコメントアウト ♻️

--- app/controllers/tils_controller.rb
+++ app/controllers/tils_controller.rb
@@ -3,25 +3,34 @@ class TilsController < ApplicationController
   before_action :set_task
   before_action :ensure_done
 
-  def new
-    @client = GithubService.new(current_user.github_token)
-    @repositories = @client.list_repositories
+  # 将来的な実装: README.md編集機能
+  # def edit
+  #   @client = GithubService.new(current_user.github_token)
+  #   @repositories = @client.list_repositories
+  #
+  #   @selected_repo = params[:repo].presence
+  #   if @selected_repo
+  #     readme = @client.fetch_readme(@selected_repo)
+  #     @readme_sha = readme[:sha]
+  #     @readme_body = readme[:body]
+  #   end
+  # rescue Octokit::Unauthorized
+  #   redirect_to @task, alert: "GitHub連携が必要です"
+  # end
+  #
+  # def update
+  #   # ✨ リポジトリ内にREADME.mdがない場合の処理も今後実装予定
+  #   client = GithubService.new(current_user.github_token)
+  #   client.update_readme(params[:repo], message: params[:message], new_body: params[:body], sha: params[:sha])
+  #   redirect_to @task, notice: "GitHubリポジトリにTILを保存しました"
+  # end
 
-    @selected_repo = params[:repo].presence
-    if @selected_repo
-      readme = @client.fetch_readme(@selected_repo)
-      @readme_sha = readme[:sha]
-      @readme_body = readme[:body]
-    end
-  rescue Octokit::Unauthorized
-    redirect_to @task, alert: "GitHub連携が必要です"
+  def new
+    # TODO: 新規.mdファイル作成機能を実装
   end
 
   def create
-    # ✨ リポジトリ内にREADME.mdがない場合の処理も今後実装予定
-    client = GithubService.new(current_user.github_token)
-    client.update_readme(params[:repo], message: params[:message], new_body: params[:body], sha: params[:sha])
-    redirect_to @task, notice: "GitHubリポジトリにTILを保存しました"
+    # TODO: 新規.mdファイル作成機能を実装
   end
 
   private
