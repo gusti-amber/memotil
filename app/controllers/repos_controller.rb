0f03ada6fe001c890fe66cd0f1edc266167387f7
Amber <amberseia1212@gmail.com>
2026-01-13 11:52:20 +0900

    feat: before_actionにタスクのステータスフィルターを追加 ✨

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,6 +1,7 @@
 class ReposController < ApplicationController
   before_action :authenticate_user!
   before_action :set_task
+  before_action :ensure_done
 
   def new
   end
@@ -14,5 +15,9 @@ class ReposController < ApplicationController
     @task = current_user.tasks.find(params[:task_id])
   rescue ActiveRecord::RecordNotFound
     redirect_to tasks_path, alert: "アクセス権限がありません"
+  end
+
+  def ensure_done
+    redirect_to @task, alert: "完了したタスクのみリポジトリを作成できます" unless @task&.done?
   end
 end
