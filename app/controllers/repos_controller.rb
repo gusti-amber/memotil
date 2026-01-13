54e82b6679d7b7cb88ab4ddc1cbcfa62492f5674
Amber <amberseia1212@gmail.com>
2026-01-13 11:51:35 +0900

    feat: before_actionにset_taskを追加 ✨

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,9 +1,18 @@
 class ReposController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_task
 
   def new
   end
 
   def create
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:task_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to tasks_path, alert: "アクセス権限がありません"
   end
 end
