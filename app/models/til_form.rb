cf16bec662065ded9edc529f77763b76b5e3a45a
Amber <amberseia1212@gmail.com>
2026-01-22 19:16:45 +0900

    feat: pathは.mdで終わるバリデーションを追加 ✨

--- app/models/til_form.rb
+++ app/models/til_form.rb
@@ -9,4 +9,14 @@ class TilForm
 
   validates :path, presence: true
   validates :repo, presence: true
+
+  validate :validate_path_format
+
+  private
+
+  def validate_path_format
+    return if path.blank?
+
+    errors.add(:path, "は.mdで終わる必要があります") unless path.end_with?(".md")
+  end
 end
