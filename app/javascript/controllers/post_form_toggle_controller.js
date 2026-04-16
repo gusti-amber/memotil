d063ce9be74902f7c489b877c48b83584229f221
Amber <yuya.ike23@gmail.com>
2026-04-16 11:26:15 +0900

    ✨ feat: トグルによるポストフォームの切り替え機能を実装

---
+++ app/javascript/controllers/post_form_toggle_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["textForm", "documentForm", "toggle"];
+  static values = { initialType: String };
+
+  connect() {
+    // ポストフォームの初期状態が"DocumentPost"の場合、isDocumentをtrueに設定
+    // 以降、isDocumentはToggleのchecked状態と同期する
+    const isDocument = this.initialTypeValue === "DocumentPost";
+    this.render(isDocument);
+  }
+
+  toggle(event) {
+    this.render(event.currentTarget.checked);
+  }
+
+  render(isDocument) {
+    // 2つのフォームの表示/非表示を切り替え
+    this.textFormTarget.classList.toggle("hidden", isDocument);
+    this.documentFormTarget.classList.toggle("hidden", !isDocument);
+
+    // 2つのフォームのトグルのchecked状態を更新
+    this.toggleTargets.forEach((toggle) => {
+      toggle.checked = isDocument;
+    });
+  }
+}
