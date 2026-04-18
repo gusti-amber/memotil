99d2051a684971af7c6ab2002f31704ba7cbc1a2
Amber <yuya.ike23@gmail.com>
2026-04-18 15:35:58 +0900

    ♻️ refactor: lintチェックを修正

--- db/migrate/20260418120000_add_postable_index_to_posts.rb
+++ db/migrate/20260418120000_add_postable_index_to_posts.rb
@@ -1,5 +1,5 @@
 class AddPostableIndexToPosts < ActiveRecord::Migration[7.2]
   def change
-    add_index :posts, [:postable_type, :postable_id]
+    add_index :posts, [ :postable_type, :postable_id ]
   end
 end
