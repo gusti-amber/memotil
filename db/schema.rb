dbba03c9f081767f249197e68670b8cccc3dae47
Amber <yuya.ike23@gmail.com>
2026-04-18 15:47:47 +0900

    🔧 fix: documentsテーブルへのカラム追加を行い、スキーマを変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_18_120000) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_18_130100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_18_120000) do
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
     t.index ["url"], name: "index_documents_on_url", unique: true
   end
 
